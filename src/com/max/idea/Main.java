9b79807e8b5ecf5850c2c116d0c87559fbe6ebb8
Yuliya <YEvgrafova@IBS.ru>
2021-11-07 20:38:23 +0300

    Задание 2

--- src/com/max/idea/Main.java
+++ src/com/max/idea/Main.java
@@ -1,7 +1,12 @@
 package com.max.idea;
 
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        double myDouble = 1.7E+308;//Задание 1
+        byte myByte = 127;//Задание 2
+        System.out.println(myDouble);
+        System.out.println(myByte);
     }
 }
