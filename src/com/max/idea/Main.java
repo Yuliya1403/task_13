10792f05a65e9d36fb36016f6f3ae3aa04e2be34
Yuliya <YEvgrafova@IBS.ru>
2021-11-09 23:03:12 +0300

    Задание 3

--- src/com/max/idea/Main.java
+++ src/com/max/idea/Main.java
@@ -3,10 +3,14 @@ package com.max.idea;
 import java.sql.SQLOutput;
 
 public class Main {
-    public static void main(String[] args) {
-        double myDouble = 1.7E+308;//Задание 1
-        byte myByte = 127;//Задание 2
-        System.out.println(myDouble);
-        System.out.println(myByte);
+    public static void main(String[] args){
+            byte array [] = {1, 2, 3, 4, 5}; // Задание 1
+        byte temp = array [0];
+        array [0]=array [4];
+        array [4]= temp; // Задание 2
+        byte total = 0;
+        for ( int i = 0; i < array.length; i++) {
+        total+=array[i];}
+    System.out.println ("Сумма чисел массива: " + total) ; // Задание 3
     }
 }
