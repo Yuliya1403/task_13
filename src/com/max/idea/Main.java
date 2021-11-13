bdda5def67bae25db767ed04f462bbefcf8caef1
Yuliya <YEvgrafova@IBS.ru>
2021-11-13 19:12:39 +0300

    Задание 3 с 3-мя пунктами, отредактировала

--- src/com/max/idea/Main.java
+++ src/com/max/idea/Main.java
@@ -3,12 +3,16 @@ package com.max.idea;
 import java.sql.SQLOutput;
 
 public class Main {
-    public static void main(String[] args){
-            byte array [] = {1, 2, 3, 4, 5}; // Задание 1
-        byte temp = array [0];
-        array [0]=array [4];
-        array [4]= temp; // Задание 2
-
-    System.out.println (array [0]+ array[2]) ; // Задание 3
+    public static void main(String[] args) {
+        int array[] = {1, 2, 3, 4, 5}; // Задание 1
+        int temp = array[0];
+        array[0] = array[array.length - 1];
+        array[array.length - 1] = temp; // Задание 2
+        int mid_index = Math.round(array.length / 2);
+        System.out.println(array[0] + array[mid_index]); // Задание 3
     }
-}
+    }
+
+
+
+
