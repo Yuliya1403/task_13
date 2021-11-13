96892d808cdd1192bae2acc5a874f949998029ab
Yuliya <YEvgrafova@IBS.ru>
2021-11-13 21:30:43 +0300

    Задание 5 , с 2-мя пунктами

--- src/com/max/idea/Main.java
+++ src/com/max/idea/Main.java
@@ -4,19 +4,12 @@ import java.sql.SQLOutput;
 
 public class Main {
     public static void main(String[] args) {
-        int x = 7;
-        int y = 14;
-        int z = 12;
-        int[] nums = {x,y,z}; //Задание 1
-        int result = 0;
-        for (int m : nums)
-            result += m;
-        //System.out.println("Значение среднего арифметического равно:" + result / nums.length); //Задание 2
-        int mid_index = (int)Math.floor((result / nums.length)/2); // Задание 3
-        if (mid_index > 3){
-            System.out.println("Программа выполнена корректно");}// Задание 4
+        int one = 1;
+        int two = 2;
+        if (one < two) { // Задание 1
+            System.out.println(one + "+" +  two);} //Задание 2
 
-    }
+           }
     }
 
 
