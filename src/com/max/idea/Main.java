08f5996c27111378e28e03b10f706a6595ff2655
Yuliya <YEvgrafova@IBS.ru>
2021-11-13 23:41:03 +0300

    Задание 6 , с 3-мя пунктами

--- src/com/max/idea/Main.java
+++ src/com/max/idea/Main.java
@@ -4,13 +4,46 @@ import java.sql.SQLOutput;
 
 public class Main {
     public static void main(String[] args) {
-        int one = 1;
-        int two = 2;
-        if (one < two) { // Задание 1
-            System.out.println(one + "+" +  two);} //Задание 2
+        int type = 2;
+      String typeString = null;
+        switch (type) {
+            case 1 : typeString = "Масса";
+                break;
+            case 2 : typeString = "Расстояние";
+                break;
+        }
+           System.out.println(typeString); //Задание 1
 
-           }
-    }
+        int unit = 1;
+        String unitString = null;
+        switch (unit) {
+            case 1:
+                unitString = "Метр";
+                break;
+            case 2:
+                unitString = "Миля";
+                break;
+            case 3:
+                unitString = "Ярд";
+                break;
+            case 5:
+                unitString = "Фут";
+                break;
+        }
+            System.out.println(unitString);//Задание 2
+        int x = 10;
+        String answer = null;
+        if (x == 10)
+            answer = "Метры: 10 , Мили: 0.006, Ярды: 10.94, Футы: 32.81";
+        else
+            answer = "Неверный ответ";
+            System.out.println(answer);//Задание 3
+        }
+
+        }
+
+
+
 
 
 
