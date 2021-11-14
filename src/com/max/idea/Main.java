e55f90edc18adfcf33cac4c7a32249a217d4aa9b
Yuliya <YEvgrafova@IBS.ru>
2021-11-15 00:21:05 +0300

    Задание 9 , с 3-мя пунктами

--- src/com/max/idea/Main.java
+++ src/com/max/idea/Main.java
@@ -4,28 +4,24 @@ import java.sql.SQLOutput;
 
 public class Main {
     public static void main(String[] args) {
-        int x = 10;
-        int y = 12;
-        int z = 2;
-        int[] arrays = {1, 4, 2};
-        for (int i = 0; i < arrays.length; i++) {
-            if (arrays[i] == x || arrays[i] == y || arrays[i] == z) {
-                continue;
-            }
-            for (int i2 = 0; i2 < arrays.length; i2++) {
-                if (arrays[i2] == x || arrays[i2] == y || arrays[i2] == z) {
-                    continue;
-                }
-                for (int i3 = 0; i3 < arrays.length; i3++) {
-                    if (arrays[i3] == x || arrays[i3] == y || arrays[i3] == z) {
-                        continue;
-                    }
-                }
+        double[] array = {1, 2, 3, 4, 5}; // Задание 1
+        double average = 0;
+        if (array.length > 0) {
+            double sum = 0;
+            for (int i = 0; i < array.length; i++) {
+                sum += array[i];
+                average = sum / array.length;// Задание 2
+                System.out.println(array[i] * average); //Задание 3
             }
         }
-        System.out.println("Данное значение имеется в константах");
     }
 }
+
+
+
+
+
+
 
 
 
