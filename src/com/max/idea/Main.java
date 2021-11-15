e90275c06ea6b493b0e62dc412c90471d34c33f0
Yuliya <YEvgrafova@IBS.ru>
2021-11-15 20:48:29 +0300

    Задание 8

--- src/com/max/idea/Main.java
+++ src/com/max/idea/Main.java
@@ -4,18 +4,16 @@ import java.sql.SQLOutput;
 
 public class Main {
     public static void main(String[] args) {
-        double[] array = {1, 2, 3, 4, 5}; // Задание 1
-        double average = 0;
-        if (array.length > 0) {
-            double sum = 0;
-            for (int i = 0; i < array.length; i++) {
-                sum += array[i];
-                average = sum / array.length;// Задание 2
-                System.out.println(array[i] * average); //Задание 3
-            }
+        int n = 10;
+        int sum = 0;
+        for (int i = 1; i <= n; i += 2) {
+            sum = sum + i;
         }
+        System.out.println(sum);
     }
-}
+        }
+
+
 
 
 
