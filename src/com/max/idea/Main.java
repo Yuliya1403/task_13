1f58da4aa2b1be28871153906de9ad3c3faad452
Yuliya <YEvgrafova@IBS.ru>
2021-11-21 21:47:15 +0300

    Задание 5

--- src/com/max/idea/Main.java
+++ src/com/max/idea/Main.java
@@ -6,24 +6,29 @@ public class Main  {
     public static void main(String[] args) {
         int num1;
         int num2;
-        int sum;
-        char plus;
+        int result;
+        char smb;
         Scanner reader = new Scanner(System.in);
         System.out.print("Enter two numbers: ");
         num1 = reader.nextInt();
         num2 = reader.nextInt();
-        System.out.print("\nEnter an operator (+: ");
-        plus = reader.next().charAt(0);
-        switch (plus) {
-            case '+':
-                sum = num1 + num2;
+        System.out.print("\nEnter an operator (+, -, *, /): ");
+        smb = reader.next().charAt(0);
+        switch (smb) {
+            case '+': result = num1 + num2;
+                break;
+            case '-': result = num1 - num2;
+                break;
+            case '*': result= num1 * num2;
+                break;
+            case '/': result = num1 / num2;
                 break;
             default:
                 System.out.printf("Error! Enter correct operator");
                return;
         }
         System.out.print("\nThe result is given as follows:\n");
-        System.out.printf(num1 + " " + plus + " " + num2 + " = " + sum);
+        System.out.printf(num1 + " " + smb + " " + num2 + " = " + result);
     }
 }
 
