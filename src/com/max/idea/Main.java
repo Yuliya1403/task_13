c19b5fd8301a8a9fd0e94cfb986b53c66b6cf54c
Yuliya <YEvgrafova@IBS.ru>
2021-12-06 21:35:02 +0300

    Задание 4

--- src/com/max/idea/Main.java
+++ src/com/max/idea/Main.java
@@ -14,17 +14,18 @@ public class Main {
         y = reader.nextInt();
         z = reader.nextInt(); // Задание 1
         {
-            int [] nums = {x,y,z};
-            int result = 0;
-            for (int m : nums)
+            double [] nums = {x,y,z};
+            double result = 0;
+            for (double m : nums)
                 result += m;
-            System.out.println("Значение среднего арифметического равно:" + result % nums.length);// Задание 2
-            int mid_index = (int)Math.floor((result % nums.length)%2); // Задание 3
+            System.out.println("Значение среднего арифметического равно:" + result / nums.length);// Задание 2
+            int mid_index = (int)Math.floor((result % nums.length)/2); // Задание 3
             if (mid_index > 3){
                 System.out.println("Программа выполнена корректно");}// Задание 4
-            }
         }
     }
+}
+
 
 
 
