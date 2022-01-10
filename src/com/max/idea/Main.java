baab3b71518b84f9da6345ffa7269ee8a533a491
Yuliya <YEvgrafova@IBS.ru>
2022-01-10 11:22:27 +0300

    Задание 12

--- src/com/max/idea/Main.java
+++ src/com/max/idea/Main.java
@@ -1,21 +1,28 @@
 package com.max.idea;
 //import javax.swing.*;
 import java.sql.SQLOutput;
+import java.util.Locale;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Ввести первое  число");
-        int type = input.nextInt();
-        String str = Integer.toString(type);
-        //System.out.println(str + 2); для проверки, что число преобразовалось
-        System.out.println("Ввести второе  число");
-        int type2 = input.nextInt();
-        int resultInt = Math.max(type,  type2);
-        System.out.println("Большее число:" + resultInt);
-        int resultInt2 = Math.min(type, type2);
-        double str2 = Double.valueOf(resultInt2);
-        System.out.println("Меньшее число:" + str2);
+        boolean result;
+        boolean result2;
+        boolean result3;
+        String result4;
+        String result5;
+        String result6;
+        String str = "I like Java!!!";
+        result = str.contains("Java");
+        result2 = str.startsWith("I like");
+        result3 = str.endsWith("!!!");
+        if (result == result2);
+            result4 = str.toUpperCase();
+            System.out.println(result4);
+        result5 = str.replace("a","o");
+        result6 = result5.substring(7, 11);
+        System.out.println(result6);
+
+
 
 
 
