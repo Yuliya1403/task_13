a375f8ebcfef929fe5c1190057e6c62afd804759
Yuliya <YEvgrafova@IBS.ru>
2022-01-10 22:01:53 +0300

    Задание 12

--- src/com/max/idea/Main.java
+++ src/com/max/idea/Main.java
@@ -10,24 +10,18 @@ public class Main {
         boolean result3;
         String result4;
         String result5;
-        String result6;
         String str = "I like Java!!!";
         result = str.contains("Java");
         result2 = str.startsWith("I like");
         result3 = str.endsWith("!!!");
-        if (result == result2 == result3);
-            result4 = str.toUpperCase();
-            System.out.println(result4);
-        result5 = str.replace("a","o");
-        result6 = result5.substring(7, 11);
-        System.out.println(result6);
-
-
-
-
-
-        }
+        if (result == true && result2 == true && result3 == true)
+        System.out.println(str.toUpperCase());
+        result4 = str.replace("a","o");
+        result5 = result4.substring(7, 11);
+        System.out.println(result5);
     }
+}
+
 
 
 
