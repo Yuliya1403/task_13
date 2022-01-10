f175c7ae7ff328591d7b1b125d4c0284c2429f4b
Yuliya <YEvgrafova@IBS.ru>
2022-01-10 08:49:47 +0300

    Задание 11

--- src/com/max/idea/Main.java
+++ src/com/max/idea/Main.java
@@ -1,51 +1,45 @@
 package com.max.idea;
-
 //import javax.swing.*;
 import java.sql.SQLOutput;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Выберите что переводить: 1 - масса, 2 - расстояние");
+        System.out.println("Ввести первое  число");
         int type = input.nextInt();
-        if (type == 1) {
-            System.out.println("Выберите единицу измерения: 1 - граммы, 2 - килограммы, 3 - центнер, 4 - тонна");
-            int sum = input.nextInt();
-            if (sum < 4) {
-                System.out.println("Введите число");
-            } else if (sum > 0) {
-                System.out.println("Введите число");
-            }
-            double gr = 0;
-            double centner = 0;
-            double tonn = 0;
-            double kg = 0;
-            double variable = input.nextInt();
-            gr = kg * 0.001;
-            centner = kg * 100;
-            tonn = kg * 1000;
-            System.out.printf("килограмм:" + variable + " граммы:" + gr + " центнер:" + centner + " фут:" + tonn);
+        String str = Integer.toString(type);
+        //System.out.println(str + 2); для проверки, что число преобразовалось
+        System.out.println("Ввести второе  число");
+        int type2 = input.nextInt();
+        int resultInt = Math.max(type,  type2);
+        System.out.println("Большее число:" + resultInt);
+        int resultInt2 = Math.min(type, type2);
+        double str2 = Double.valueOf(resultInt2);
+        System.out.println("Меньшее число:" + str2);
 
 
-        } else if (type == 2) {
-            System.out.println("Выберите единицу измерения:  1 - метр, 2 - миля, 3 - ярд, 4 - фут");
+
         }
-        int sum = input.nextInt();
-        if (sum < 4) {
-            System.out.println("Введите число");
-        } else if (sum > 0) {
-            System.out.println("Введите число");
-            }
-        double milles = 0;
-        double yard = 0;
-        double foot = 0;
-            double variable = input.nextInt();
-        milles = variable * 0.62;
-        yard = variable * 1.09;
-        foot = variable * 3.28;
-                System.out.printf("метры:" + variable + " милли:" + milles + " ярд:" + yard + " фут:" + foot);
-            }
-        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
