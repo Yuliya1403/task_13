5cdf4c748b4479ecac3f81675f6f613302718f8d
Yuliya <YEvgrafova@IBS.ru>
2022-01-30 23:22:56 +0300

    Задание 10

--- src/com/max/idea/Main.java
+++ src/com/max/idea/Main.java
@@ -5,39 +5,30 @@ import java.util.Locale;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        int[] array = new int[15];
+        Scanner input = new Scanner(System.in);
 
-        for (int i = 0; i < array.length; i++) {
-            array[i] = ((int) (Math.random() * 41) - 20);
-        }
+        System.out.println("Введите ширину матрицы");
+        int width = input.nextInt();
 
-        int maxValue = array[0];
-        for (int i = 1; i < array.length; i++) {
-            if (array[i] > maxValue) {
-                maxValue = array[i];
+        System.out.println("Введите высоту матрицы");
+        int height = input.nextInt();
+
+        int[][] matrix = new int[width][height];
+
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = 0; j < matrix.length; j++) {
+                System.out.print("Введите элементы матрицы " + i + ", " + j);
+                matrix[i][j] = input.nextInt();
             }
         }
-        System.out.println(maxValue);
 
-        int minValue = array[0];
-        for (int i = 1; i < array.length; i++) {
-            if (array[i] < minValue) {
-                minValue = array[i];
-            }
-        }
-        System.out.println(minValue);
-
-        int maxValueAbs = Math.abs(maxValue);
-        int minValueAbs = Math.abs(minValue);
-
-        if (maxValueAbs >= minValueAbs) {
-            System.out.println(maxValueAbs);
-        } else {
-            System.out.println(minValueAbs);
+        for(int l = 0; l < matrix[0].length; l++) {
+            System.out.println(matrix[0][l] * 3);
         }
 
-                }
-            }
+    }
+}
+
 
 
 
