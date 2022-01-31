f8f765c27cadfc35e2e6b15ea59b3ce7d7735495
Yuliya <YEvgrafova@IBS.ru>
2022-01-31 12:42:40 +0300

    Задание 4

--- src/com/max/idea/Main.java
+++ src/com/max/idea/Main.java
@@ -5,14 +5,27 @@ import java.util.Locale;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        int array[] = {1, 2, 3, 4, 5}; // Задание 1
-        int temp = array[0];
-        array[0] = array[array.length - 1];
-        array[array.length - 1] = temp; // Задание 2
-        int mid_index = (array.length / 2);
-        System.out.println(array[0] + array[mid_index]); // Задание 3
+        double x ;
+        double y ;
+        double z ;
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter tree numbers: ");
+        x = reader.nextInt();
+        y = reader.nextInt();
+        z = reader.nextInt(); // Задание 1
+        {
+            double [] nums = {x,y,z};
+            double result = 0;
+            for (double m : nums)
+                result += m;
+            System.out.println("Значение среднего арифметического равно:" + (double) result / nums.length);// Задание 2
+            double mid_index = (double) Math.floor((result / nums.length)/2); // Задание 3
+            if (mid_index > 3){
+                System.out.println("Программа выполнена корректно");}// Задание 4
+        }
     }
     }
+
 
 
 
