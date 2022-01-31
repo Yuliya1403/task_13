a8846eb4576d8c2034628150b81712cc467d14e6
Yuliya <YEvgrafova@IBS.ru>
2022-01-31 09:19:24 +0300

    Задание 2

--- src/com/max/idea/Main.java
+++ src/com/max/idea/Main.java
@@ -5,9 +5,12 @@ import java.util.Locale;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-                System.out.println("hello world");
-            }
+            double myDouble = 1.7E+308;//Задание 1
+            byte myByte = 127;//Задание 2
+            System.out.println(myDouble);
+            System.out.println(myByte);
         }
+    }
 
 
 
