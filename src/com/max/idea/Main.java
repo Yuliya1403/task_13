36a6fd3421d6f204e5f21287a3995ee33de9ca30
Yuliya <YEvgrafova@IBS.ru>
2022-01-31 09:15:06 +0300

    Задание 1

--- src/com/max/idea/Main.java
+++ src/com/max/idea/Main.java
@@ -5,29 +5,9 @@ import java.util.Locale;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-
-        System.out.println("Введите ширину матрицы");
-        int width = input.nextInt();
-
-        System.out.println("Введите высоту матрицы");
-        int height = input.nextInt();
-
-        int[][] matrix = new int[width][height];
-
-        for(int i = 0; i < matrix.length; i++) {
-            for(int j = 0; j < matrix.length; j++) {
-                System.out.print("Введите элементы матрицы " + i + ", " + j);
-                matrix[i][j] = input.nextInt();
+                System.out.println("hello world");
             }
         }
-
-        for(int l = 0; l < matrix[0].length; l++) {
-            System.out.println(matrix[0][l] * 3);
-        }
-
-    }
-}
 
 
 
