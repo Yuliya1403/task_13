594c180ed7418fe3980eb4e641cac394bb48bc42
Yuliya <YEvgrafova@IBS.ru>
2022-02-08 22:18:32 +0300

    Задание 8

--- src/com/max/idea/Main.java
+++ src/com/max/idea/Main.java
@@ -5,65 +5,16 @@ import java.util.Locale;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        int result = 0;
-        int num1;
-        int num2;
-        char smb;
-        num1 = ((int) (Math.random() * 9));
-        num2 = ((int) (Math.random() * 9));
-        Scanner input = new Scanner(System.in);
-        System.out.println("Выберите тип уравнения: 1 - x в начале, 2 - x в середине, 3 - x в равенстве");
-        int type = input.nextInt();
-        if (type == 1) {
-            System.out.print("\nВведите знак (+, -): ");
-            smb = input.next().charAt(0);
-            switch (smb) {
-                case '+':
-                    result = num2 - num1;
-                    break;
-                case '-':
-                    result = num1 + num2;
-                    break;
-                default:
-                    System.out.printf("Неккоректно введены данные");
-            }
-
-            System.out.println("первое число равно:" + num1 + " второе число равно:" + num2 + ", результат:" + result);
-
+        int num;
+        int sum = 0;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите число: ");
+        num = in.nextInt();
+        for (int i = 1; i <= num; i += 2) {
+            sum = sum + i;
         }
-        if (type == 2) {
-            System.out.print("\nВведите знак (+, -): ");
-            smb = input.next().charAt(0);
-            switch (smb) {
-                case '+':
-                    result = num2 - num1;
-                    break;
-                case '-':
-                    result = num1 + num2;
-                    break;
-                default:
-                    System.out.printf("Неккоректно введены данные");
-            }
+        System.out.println(sum);
 
-            System.out.println(" первое число равно:" + num1 + " второе число равно:" + num2 + ", результат:" + result);
-
-        }
-        if (type == 3) {
-            System.out.print("\nВведите знак (+, -): ");
-            smb = input.next().charAt(0);
-            switch (smb) {
-                case '+':
-                    result = num2 + num1;
-                    break;
-                case '-':
-                    result = num1 - num2;
-                    break;
-                default:
-                    System.out.printf("Неккоректно введены данные");
-            }
-
-            System.out.println("первое число равно:" + num1 + " второе число равно:" + num2 + " результат:" + result);
-        }
     }
 }
 
